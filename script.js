8c2ade1545aa55eb3d21ef3a4cb134c9d39090a8
Angel <Asturcharger@gmail.com>
2022-05-30 14:19:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+/*function splitString(sentence){
+    var words = sentence.split(" ");
+    console.log(words[0]);
+    console.log(words[1]);
+}
+
+splitString("hablar guardia");
+splitString("abrir puerta");*/
+
+var nuevoArray = new Array(4);
+nuevoArray[0] = new Array(4);
+nuevoArray[1] = new Array(4);
+nuevoArray[2] = new Array(4);
+nuevoArray[3] = new Array(4);
+
+nuevoArray[0][0]="Cobra.Te muerde una cobra y mueres de terribles sufrimientos"
+nuevoArray[0][1]="Encuentras la llave de un cofre"
+nuevoArray[0][2]="Encuentras una cabaña junto al mar"
+nuevoArray[0][3]="Inicio en una isla del caribe. Eres un pirata en búsqueda de un tesoro."
+nuevoArray[1][0]="Comienzas a navegar en la dirección con el cielo más despejado (Sur)"
+nuevoArray[1][1]="El camino lleva a un embarcadero. En él, hay un pequeño barco velero. TE subes al barco"
+nuevoArray[1][2]="Descubres un camino que sale de la cabaña"
+nuevoArray[1][3]="No encuentras un tesoro. Pero sí una clave privada. Podría ser una clave de Bitcoin!"
+nuevoArray[2][0]="Una tormenta hunde tu barco. Te quedas flotando agarrado a un listón de madera"
+nuevoArray[2][1]="Nadando hacia el Este, consigues llegar a nado a otra isla"
+nuevoArray[2][2]="Llegas a una cabaña con una puerta cerrada"
+nuevoArray[2][3]="Sala-comedor llena de esqueletos de piratas que murieron envenenados. Al norte hay una puerta y otra al sur."
+nuevoArray[3][0]="Nadando hacia el Sur no encuentras nada y te ahogas."
+nuevoArray[3][1]="Si vas al sur de la isla te encuentras una zona selvática"
+nuevoArray[3][2]="Encuentras colgada de un árbol una llave grande dorada que parece para abrir una puerta"
+nuevoArray[3][3]="Si abres la puerta sur, una banda de piratas recien llegados te roban todo lo que tienes y te matan"
+
+console.log(nuevoArray[0][0])
